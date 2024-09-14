fb9289ba56a41d7a1bebb2442ea0fa13bb180cb1
ShrivarshiniKM <shrikm97@gmail.com>
2024-09-14 11:41:06 +0530

    Uploaded Rest Assured activities

--- Rest_Assured/Activities/Activity2.java
+++ Rest_Assured/Activities/Activity2.java
@@ -1,56 +1,51 @@
 package activities;
 
-import org.testng.annotations.Test;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import org.testng.annotations.Test;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Activity2 {
-	 // Set base URL
     final static String ROOT_URI = "https://petstore.swagger.io/v2/user";
- 
+
     @Test(priority=1)
     public void addNewUserFromFile() throws IOException {
-        // Import JSON file
         FileInputStream inputJSON = new FileInputStream("src/test/java/activities/userinfo.json");
-        // Read JSON file as String
         String reqBody = new String(inputJSON.readAllBytes());
- 
+
         Response response = 
-            given().contentType(ContentType.JSON) // Set headers
-            .body(reqBody) // Pass request body from file
-            .when().post(ROOT_URI); // Send POST request
- 
+            given().contentType(ContentType.JSON)
+            .body(reqBody) 
+            .when().post(ROOT_URI);
+
         inputJSON.close();
- 
+
         // Assertion
         response.then().body("code", equalTo(200));
-        response.then().body("message", equalTo("1083"));
+        response.then().body("message", equalTo("2705"));
     }
     
     @Test(priority=2)
     public void getUserInfo() {
-        // Import JSON file to write to
         File outputJSON = new File("src/test/java/activities/userGETResponse.json");
- 
+
         Response response = 
-            given().contentType(ContentType.JSON) // Set headers
-            .pathParam("username", "Reeta") // Pass request body from file
-            .when().get(ROOT_URI + "/{username}"); // Send POST request
+            given().contentType(ContentType.JSON)
+            .pathParam("username", "ShrivarshiniKM")
+            .when().get(ROOT_URI + "/{username}");
         
         // Get response body
         String resBody = response.getBody().asPrettyString();
- 
+
         try {
-            // Create JSON file
             outputJSON.createNewFile();
-            // Write response body to external file
             FileWriter writer = new FileWriter(outputJSON.getPath());
             writer.write(resBody);
             writer.close();
@@ -58,25 +53,23 @@ public class Activity2 {
             excp.printStackTrace();
         }
         
-        // Assertion
-        response.then().body("id", equalTo(1083));
-        response.then().body("username", equalTo("Reeta"));
-        response.then().body("firstName", equalTo("Reeta"));
-        response.then().body("lastName", equalTo("johne"));
-        response.then().body("email", equalTo("reeta@mail.com"));
-        response.then().body("password", equalTo("password123"));
-        response.then().body("phone", equalTo("9812763346"));
+        response.then().body("id", equalTo("2705"));
+        response.then().body("username", equalTo("ShrivarshiniKM"));
+        response.then().body("firstName", equalTo("Shrivarshini"));
+        response.then().body("lastName", equalTo("KM"));
+        response.then().body("email", equalTo("shrikm@mail.com"));
+        response.then().body("password", equalTo("password12345"));
+        response.then().body("phone", equalTo("9823443450"));
     }
     
     @Test(priority=3)
     public void deleteUser() throws IOException {
         Response response = 
-            given().contentType(ContentType.JSON) // Set headers
-            .pathParam("username", "Reeta") // Add path parameter
-            .when().delete(ROOT_URI + "/{username}"); // Send POST request
- 
-        // Assertion
+            given().contentType(ContentType.JSON)
+            .pathParam("username", "justinc")
+            .when().delete(ROOT_URI + "/{username}");
+
         response.then().body("code", equalTo(200));
-        response.then().body("message", equalTo("Reeta"));
+        response.then().body("message", equalTo("ShrivarshiniKM"));
     }
 }
