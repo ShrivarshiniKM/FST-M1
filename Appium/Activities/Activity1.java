6a8090ab90dabd0d9591b34619705d4d7f9c8908
ShrivarshiniKM <shrikm97@gmail.com>
2024-09-18 16:55:23 +0530

    Uploaded latest files

--- Appium/Activities/Activity1.java
+++ Appium/Activities/Activity1.java
@@ -1,49 +1,57 @@
-package activities;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+package Activities;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Activity1 {
-	AndroidDriver driver;
+    
+    AndroidDriver driver;
 
-	@BeforeClass
-	public void setup() throws MalformedURLException {
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setPlatformName("android");
-		options.setAutomationName("UiAutomator2");
-		options.setAppPackage("com.google.android.calculator");
-		options.setAppActivity("com.android.calculator2.Calculator");
-		options.noReset();
+  
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.calculator2");
+        options.setAppActivity(".Calculator");
+        options.noReset();
 
-		URL serverUrl = new URL("http://localhost:4723/");
+        
+        URL serverURL = new URL("http://localhost:4723/");
 
-		driver = new AndroidDriver(serverUrl, options);
-	}
+      
+        driver = new AndroidDriver(serverURL, options);
+    }
 
-	@Test
-	public void multiplyCase() {
-		driver.findElement(AppiumBy.accessibilityId("9")).click();
-		driver.findElement(AppiumBy.accessibilityId("multiply")).click();
-		driver.findElement(AppiumBy.accessibilityId("5")).click();
-		driver.findElement(AppiumBy.accessibilityId("equals")).click();
+    // Test method
+    @Test
+    public void multiplyTest() {
+        
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        driver.findElement(AppiumBy.id("digit_8")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
 
-		String result = driver.findElement(AppiumBy.id("result_final")).getText();
+        String result = driver.findElement(AppiumBy.id("result")).getText();
 
-		Assert.assertEquals(result, "45");
-	}
+        
+        Assert.assertEquals(result, "40");
+    }
 
-	@AfterClass
-	public void tearDown() {
-		driver.quit();
-	}
+
+    // Tear down method
+    @AfterClass
+    public void tearDown() {
+        // Close the app
+        driver.quit();
+    }
 }
